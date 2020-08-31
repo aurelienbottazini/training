d4cfc1f937b2def0dc49e19ecfb9dd43619f3436
aurelienbottazini <aurelien.bottazini@gmail.com>
2020-08-31 15:21:51 +0200

    cleanup

--- js/test.js
+++ js/test.js
@@ -4,20 +4,15 @@ const friends = [
   { id: 3, name: 'Pink', nearMe: true}
 ]
 
-
 function isNearMe({ nearMe }) {
   console.log(nearMe);
   return nearMe;
 }
 
-
 function getName({ name }) {
   console.log(name);
   return name;
 }
-
-
-
 
 function getFriendsNearMe(x) {
   return compose(
@@ -26,15 +21,12 @@ function getFriendsNearMe(x) {
   )(x);
 }
 
-
 const compose = (...fns) => (x) => (fns.reduce((acc, cur) => cur(acc), x));
 const mapTransducer = transform => step => (acc, cur) => step(acc, transform(cur));
 const filterTransducer = filter => step => (acc, cur) => filter(cur) ? step(acc, cur) : acc;
 const concatArray = (acc, cur) => acc.concat([cur]);
 
-
 const toArray = (xform, foldable) => foldable.reduce(xform(concatArray), []);
-
 
 // need toArray to tell transducers to start the process.
 toArray(getFriendsNearMe, friends); //?
