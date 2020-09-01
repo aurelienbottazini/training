4f52b5a955d1567a15e9d390dda6a69f3c6e2eab
Aurélien Bottazini <aurelien.bottazini@gmail.com>
2020-09-01 22:17:31 +0200

    init.sh

--- ruby/test.rb
+++
@@ -1,6 +0,0 @@
-class Symbol
-  def to_proc # !> method redefined; discarding old to_proc
-    Proc.new { |obj| obj.public_send(self)}
-  end
-end
-%w{ david black }.map &:capitalize # => ["David", "Black"]
