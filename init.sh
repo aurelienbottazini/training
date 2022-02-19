cad825255d9af9ec3eeb8be29b608e978e43bb7a
Aurélien Bottazini <aurelien.bottazini@gmail.com>
2022-02-19 21:24:09 +0000

    refactor: move init.sh to correct place

--- init.sh
+++ init.sh
@@ -1,5 +1,4 @@
 touch index.html
 touch ruby/ruby.rb
 touch js/javascript.js
-touch clojure/src/training.clj
-touch styles.css
+touch src/clj_training/clojure.clj
--- js/init.sh
+++
@@ -1,4 +0,0 @@
-touch index.html
-touch ruby/ruby.rb
-touch js/javascript.js
-touch src/clj_training/clojure.clj
