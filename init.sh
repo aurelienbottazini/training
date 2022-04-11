a767de394010b47f5623c2806cafe379aa8f11fa
Aurélien Bottazini <aurelien.bottazini@gmail.com>
2022-04-11 07:23:44 +0200

    refator: clojure training files

--- init.sh
+++ init.sh
@@ -1,5 +1,5 @@
 touch index.html
 touch ruby/ruby.rb
 touch js/javascript.js
-mkdir -p src/clj_training
-touch src/clj_training/clojure.clj
+mkdir -p clojure/src/clj_training
+touch clojure/src/clj_training/clojure.clj
