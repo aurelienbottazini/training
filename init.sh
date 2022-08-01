1cdc75dee5376091c6067143966a079b74d69f1f
Aurélien Bottazini <aurelien.bottazini@gmail.com>
2022-08-01 22:49:27 +0200

    init.sh typescript

--- init.sh
+++ init.sh
@@ -1,6 +1,7 @@
 touch index.html
 touch ruby/ruby.rb
 touch js/javascript.js
+touch js/typescript.js
 mkdir -p clojure/src/clj_training
 touch clojure/src/clj_training/clojure.clj
 touch markdown.md
